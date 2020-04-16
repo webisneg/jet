e0f83c58d31f46b38592b986c5d035ebb816c8f5
bilatchuk1994 <sefan1994@gmail.com>
2020-04-16 20:49:25 +0300

    Правки і Страница Пароля

---
+++ reset-password.php
@@ -0,0 +1,55 @@
+<?php include("header.php"); ?>
+
+<main class="main">
+
+<section class="catalog-top about-top">
+  <div class="container">
+    <div class="row">
+      <div class="col-lg-12">
+        <div class="title-page">
+          <h1>Reset Password</h1>
+        </div>
+        <div class="title-divider"></div>
+      </div>
+    </div>
+    </div>
+  </div>
+</section>
+
+
+<section class="about reset-pass">
+  <div class="container">
+    <div class="row">
+      <div class="col-lg-12">
+        <div class="popup-block">
+      <div>
+        <form action="" method="POST">
+          <input type="email" placeholder="E-Mail Address">
+          <input type="email" placeholder="Новый пароль">
+          <input type="email" placeholder="Подтверждение">
+          <div class="forgot-pass">
+        <a href="">Забыли пароль?</a>
+      </div>
+          <button>Send Password Reset Link</button>
+        </form>
+      </div>
+      <div class="close-btn">
+          <img src="img/close.svg" alt="">
+        </div>
+    </div>
+      </div>
+    </div>
+  </div>
+</section>
+
+
+
+</main>
+
+
+
+<?php include("footer.php"); ?>
+
+
+
+
