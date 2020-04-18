645f3bba6a7664e100351541a135f7a5a1e28c52
bilatchuk1994 <sefan1994@gmail.com>
2020-04-18 20:31:42 +0300

    123

--- reset-password.php
+++ reset-password.php
@@ -35,6 +35,20 @@
           <img src="img/close.svg" alt="">
         </div>
     </div>
+    <div class="popup-block">
+          <div class="popup-title">
+            <h2>Ссылка для восстановления была отправлена на ваш Email адрес</h2>
+      </div>
+      <div>
+        <form action="" method="POST">
+          <input type="email" placeholder="E-Mail Address">
+          <button>восстановить пароль</button>
+        </form>
+      </div>
+      <div class="close-btn">
+          <img src="img/close.svg" alt="">
+        </div>
+    </div>
       </div>
     </div>
   </div>
