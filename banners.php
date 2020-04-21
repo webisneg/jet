b1eb15a2c64087242da72747c5841fd2a2e68585
bilatchuk1994 <sefan1994@gmail.com>
2020-04-21 19:28:56 +0300

    добавление баннеров

---
+++ banners.php
@@ -0,0 +1,129 @@
+<?php include("header.php"); ?>
+
+<main class="main">
+
+<section class="catalog-top about-top">
+  <div class="container">
+    <div class="row">
+      <div class="col-lg-12">
+        <div class="title-page">
+          <h1>Reset Password</h1>
+        </div>
+        <div class="title-divider"></div>
+      </div>
+    </div>
+    </div>
+  </div>
+</section>
+
+
+<section class="banner">
+  <div class="container">
+    <div class="row">
+      <div class="col-8 col-lg-7 col-md-8">
+        <h3>Ликвидации акссесуаров <br> к компютерам и ноутбукам от крупнейших ритейлеров</h3>
+        <p>Покупайте выгодно и зарабатывайте от 100% прибыли на их перепродажи</p>
+      </div>
+      <div class="col-4 col-lg-5 col-md-4">
+        <div class="banner-img">
+          <img src="img/category-img.png" alt="">
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+
+
+<section class="banner banner-black banner-tablet">
+  <div class="container">
+    <div class="row">
+      <div class="col-8 col-lg-7 col-md-8">
+        <h3>Ликвидации акссесуаров <br> к планшетам от крупнейших ритейлеров</h3>
+        <p>Покупайте выгодно и зарабатывайте от 100% прибыли на их перепродажи</p>
+      </div>
+      <div class="col-4 col-lg-5 col-md-4">
+        <div class="banner-img">
+          <img src="img/category-img2.png" alt="">
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+
+<section class="banner banner-mobile">
+  <div class="container">
+    <div class="row">
+      <div class="col-8 col-lg-7 col-md-8">
+        <h3>Ликвидации акссесуаров <br> к телефонам от крупнейших ритейлеров</h3>
+        <p>Покупайте выгодно и зарабатывайте от 100% прибыли на их перепродажи</p>
+      </div>
+      <div class="col-4 col-lg-5 col-md-4">
+        <div class="banner-img">
+          <img src="img/category-img3.png" alt="">
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+
+
+<section class="banner banner-catalog">
+  <div class="container">
+    <div class="row">
+      <div class="col-8 col-lg-7 col-md-8">
+        <h3>Ликвидации товаров</h3>
+        <p>Покупайте выгодно и зарабатывайте от 100% прибыли на их перепродажи</p>
+      </div>
+      <div class="col-4 col-lg-5 col-md-4">
+        <div class="banner-img">
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+
+
+<section class="banner banner-headphone">
+  <div class="container">
+    <div class="row">
+      <div class="col-8 col-lg-7 col-md-8">
+        <h3>Ликвидации наушников крупнейших ритейлеров</h3>
+        <p>Зарабатывай на их перепродажи от 100%</p>
+      </div>
+      <div class="col-4 col-lg-5 col-md-4">
+        <div class="banner-img">
+          <img src="img/category-img5.png" alt="">
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+
+
+<section class="banner banner-other">
+  <div class="container">
+    <div class="row">
+      <div class="col-8 col-lg-7 col-md-8">
+        <h3>Ликвидации прочих акссесуаров от крупнейших  ритейлеров</h3>
+        <p>Покупайте выгодно и зарабатывайте от 100% прибыли на их перепродажи</p>
+      </div>
+      <div class="col-4 col-lg-5 col-md-4">
+        <div class="banner-img">
+          <img src="img/category-img4.png" alt="">
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+
+
+
+</main>
+
+
+
+<?php include("footer.php"); ?>
+
+
+
+
