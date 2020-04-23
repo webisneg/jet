b9869f4a7512582e227b91581a9c183bcc189155
bilatchuk1994 <sefan1994@gmail.com>
2020-04-23 16:29:28 +0300

    Баннер на тсраницу горячие лоты

--- banners.php
+++ banners.php
@@ -194,6 +194,35 @@
   </div>
 </section>
 
+<section class="banner banner-hot">
+  <div class="container">
+    <div class="row">
+        <div class="col-lg-12">
+          <div class="breadcrums">
+          <ul>
+            <li><a href="">Главная</a></li>
+            <li><span>></span></li>
+            <li><a href="">каталог</a></li>
+            <li><span>></span></li>
+            <li>Гарнитуры для PS4 Pro / PS4. Xbox One & More. 951 Единицы</li>
+          </ul>
+        </div>
+        </div>
+      </div>
+    <div class="row">
+      <div class="col-8 col-lg-7 col-md-8">
+        <h3>Самые горячие аукционы ликвидаций от крупнейших ритейлеров</h3>
+        <p>Покупайте выгодно и зарабатывайте от 100% прибыли на их перепродажи</p>
+      </div>
+      <div class="col-4 col-lg-5 col-md-4">
+        <div class="banner-img">
+          <img src="img/category-img6.png" alt="">
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+
 
 
 </main>
