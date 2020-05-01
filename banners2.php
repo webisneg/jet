f4f6879a51052c4cf2db1db1ff2cb131598c580a
bilatchuk1994 <sefan1994@gmail.com>
2020-05-01 12:47:14 +0300

    Баннера

---
+++ banners2.php
@@ -0,0 +1,267 @@
+<?php include("header.php"); ?>
+
+<main class="main">
+
+<section class="catalog-top about-top">
+  <div class="container">
+    <div class="row">
+      <div class="col-lg-12">
+        <div class="title-page">
+          <h1>Reset Password</h1>
+        </div>
+        <div class="title-divider"></div>
+      </div>
+    </div>
+    </div>
+  </div>
+</section>
+
+
+<section class="banner banner-hot-clothing">
+  <div class="container">
+    <div class="row">
+        <div class="col-lg-12">
+          <div class="breadcrums">
+          <ul>
+            <li><a href="">Главная</a></li>
+            <li><span>></span></li>
+            <li><a href="">каталог</a></li>
+            <li><span>></span></li>
+            <li>Гарнитуры для PS4 Pro / PS4. Xbox One & More. 951 Единицы</li>
+          </ul>
+        </div>
+        </div>
+      </div>
+    <div class="row">
+      <div class="col-8 col-lg-7 col-md-8">
+        <h3>Ликвидации горячих лотов  крупнейших fashion  ритейлеров</h3>
+        <p>Покупайте выгодно и зарабатывайте от 100% прибыли на их перепродажи</p>
+      </div>
+      <div class="col-4 col-lg-5 col-md-4">
+        <div class="banner-img">
+          <img src="img/category-img11.png" alt="">
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+
+
+<section class="banner banner-clothing">
+  <div class="container">
+    <div class="row">
+        <div class="col-lg-12">
+          <div class="breadcrums">
+          <ul>
+            <li><a href="">Главная</a></li>
+            <li><span>></span></li>
+            <li><a href="">каталог</a></li>
+            <li><span>></span></li>
+            <li>Гарнитуры для PS4 Pro / PS4. Xbox One & More. 951 Единицы</li>
+          </ul>
+        </div>
+        </div>
+      </div>
+    <div class="row">
+      <div class="col-8 col-lg-7 col-md-8">
+        <h3>Ликвидации верхней одежды от крупнейших ритейлеров</h3>
+        <p>Покупайте выгодно и зарабатывайте от 100% прибыли на их перепродажи</p>
+      </div>
+      <div class="col-4 col-lg-5 col-md-4">
+        <div class="banner-img">
+          <img src="img/category-img10.png" alt="">
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+
+<section class="banner banner-black banner-denim">
+  <div class="container">
+    <div class="row">
+        <div class="col-lg-12">
+          <div class="breadcrums">
+          <ul>
+            <li><a href="">Главная</a></li>
+            <li><span>></span></li>
+            <li><a href="">каталог</a></li>
+            <li><span>></span></li>
+            <li>Гарнитуры для PS4 Pro / PS4. Xbox One & More. 951 Единицы</li>
+          </ul>
+        </div>
+        </div>
+      </div>
+    <div class="row">
+      <div class="col-8 col-lg-7 col-md-8">
+        <h3>Ликвидации джинсов и штанов   от крупнейших ритейлеров</h3>
+        <p>Покупайте выгодно и зарабатывайте от 100% прибыли на их перепродажи</p>
+      </div>
+      <div class="col-4 col-lg-5 col-md-4">
+        <div class="banner-img">
+          <img src="img/category-img12.png" alt="">
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+
+
+<section class="banner banner-shirts">
+  <div class="container">
+    <div class="row">
+        <div class="col-lg-12">
+          <div class="breadcrums">
+          <ul>
+            <li><a href="">Главная</a></li>
+            <li><span>></span></li>
+            <li><a href="">каталог</a></li>
+            <li><span>></span></li>
+            <li>Гарнитуры для PS4 Pro / PS4. Xbox One & More. 951 Единицы</li>
+          </ul>
+        </div>
+        </div>
+      </div>
+    <div class="row">
+      <div class="col-8 col-lg-7 col-md-8">
+        <h3>Ликвидации  майек, байек, свитеров и рубашек от крупнейших ритейлеров</h3>
+        <p>Покупайте выгодно и зарабатывайте от 100% прибыли на их перепродажи</p>
+      </div>
+      <div class="col-4 col-lg-5 col-md-4">
+        <div class="banner-img">
+          <img src="img/category-img13.png" alt="">
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+
+
+<section class="banner banner-shoes">
+  <div class="container">
+    <div class="row">
+        <div class="col-lg-12">
+          <div class="breadcrums">
+          <ul>
+            <li><a href="">Главная</a></li>
+            <li><span>></span></li>
+            <li><a href="">каталог</a></li>
+            <li><span>></span></li>
+            <li>Гарнитуры для PS4 Pro / PS4. Xbox One & More. 951 Единицы</li>
+          </ul>
+        </div>
+        </div>
+      </div>
+    <div class="row">
+      <div class="col-8 col-lg-7 col-md-8">
+        <h3>Ликвидации  обуви от крупнейших ритейлеров</h3>
+        <p>Покупайте выгодно и зарабатывайте от 100% прибыли на их перепродажи</p>
+      </div>
+      <div class="col-4 col-lg-5 col-md-4">
+        <div class="banner-img">
+          <img src="img/category-img14.png" alt="">
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+
+
+<section class="banner banner-dress">
+  <div class="container">
+    <div class="row">
+        <div class="col-lg-12">
+          <div class="breadcrums">
+          <ul>
+            <li><a href="">Главная</a></li>
+            <li><span>></span></li>
+            <li><a href="">каталог</a></li>
+            <li><span>></span></li>
+            <li>Гарнитуры для PS4 Pro / PS4. Xbox One & More. 951 Единицы</li>
+          </ul>
+        </div>
+        </div>
+      </div>
+    <div class="row">
+      <div class="col-8 col-lg-7 col-md-8">
+        <h3>Ликвидации платьев, костюмов и пиджаков  от крупнейших ритейлеров</h3>
+        <p>Покупайте выгодно и зарабатывайте от 100% прибыли на их перепродажи</p>
+      </div>
+      <div class="col-4 col-lg-5 col-md-4">
+        <div class="banner-img">
+          <img src="img/category-img15.png" alt="">
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+
+<section class="banner banner-bag">
+  <div class="container">
+    <div class="row">
+        <div class="col-lg-12">
+          <div class="breadcrums">
+          <ul>
+            <li><a href="">Главная</a></li>
+            <li><span>></span></li>
+            <li><a href="">каталог</a></li>
+            <li><span>></span></li>
+            <li>Гарнитуры для PS4 Pro / PS4. Xbox One & More. 951 Единицы</li>
+          </ul>
+        </div>
+        </div>
+      </div>
+    <div class="row">
+      <div class="col-8 col-lg-7 col-md-8">
+        <h3>Ликвидации  cумок, очков и аксессуаров от крупнейших ритейлеров</h3>
+        <p>Покупайте выгодно и зарабатывайте от 100% прибыли на их перепродажи</p>
+      </div>
+      <div class="col-4 col-lg-5 col-md-4">
+        <div class="banner-img">
+          <img src="img/category-img16.png" alt="">
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+
+
+<section class="banner banner-black banner-fashion">
+  <div class="container">
+    <div class="row">
+        <div class="col-lg-12">
+          <div class="breadcrums">
+          <ul>
+            <li><a href="">Главная</a></li>
+            <li><span>></span></li>
+            <li><a href="">каталог</a></li>
+            <li><span>></span></li>
+            <li>Гарнитуры для PS4 Pro / PS4. Xbox One & More. 951 Единицы</li>
+          </ul>
+        </div>
+        </div>
+      </div>
+    <div class="row">
+      <div class="col-8 col-lg-7 col-md-8">
+        <h3>Ликвидации одежды крупнейших fashion ритейлеров</h3>
+        <p>Покупайте выгодно и зарабатывайте от 100% прибыли на их перепродажи</p>
+      </div>
+      <div class="col-4 col-lg-5 col-md-4">
+        <div class="banner-img">
+          <img src="img/category-img17.png" alt="">
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+
+
+
+</main>
+
+
+
+<?php include("footer.php"); ?>
+
+
+
+
